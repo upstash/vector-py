b798b5d9a68e235afd8b53d5a8d67121a0e7dae7
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-09-08 22:08:53 +0300

    finalize

--- upstash_vector/core/resumable_query.py
+++ upstash_vector/core/resumable_query.py
@@ -17,21 +17,54 @@ class ResumableQuery:
         self.namespace = namespace
         self.uuid = Optional[str]
 
-    async def start(self) -> Dict[str, Any]:
-        path = (
-            RESUMABLE_QUERY_VECTOR_PATH
-            if "vector" in self.payload
-            else RESUMABLE_QUERY_DATA_PATH
-        )
+    async def async_start(self) -> List[QueryResult]:
+        if "vector" in self.payload:
+            path = RESUMABLE_QUERY_VECTOR_PATH
+        elif "data" in self.payload:
+            path = RESUMABLE_QUERY_DATA_PATH
+        else:
+            raise ClientError("Payload must contain either 'vector' or 'data' key.")
+
         if self.namespace:
-            path = f"{self.namespace}/{path}"
+            path = f"{path}/{self.namespace}"
+
         result = await self.client._execute_request_async(
             payload=self.payload, path=path
         )
         self.uuid = result["uuid"]
-        return result
+        return result["scores"]
 
-    async def fetch_next(self, additional_k: int) -> List[QueryResult]:
+    def start(self) -> List[QueryResult]:
+        if "vector" in self.payload:
+            path = RESUMABLE_QUERY_VECTOR_PATH
+        elif "data" in self.payload:
+            path = RESUMABLE_QUERY_DATA_PATH
+        else:
+            raise ClientError("Payload must contain either 'vector' or 'data' key.")
+
+        if self.namespace:
+            path = f"{path}/{self.namespace}"
+
+        result = self.client._execute_request(payload=self.payload, path=path)
+
+        self.uuid = result["uuid"]
+        if not self.uuid:
+            raise ClientError("Resumable query could not be started.")
+
+        return [QueryResult._from_json(obj) for obj in result.get("scores", [])]
+
+    def fetch_next(self, additional_k: int) -> List[QueryResult]:
+        if self.uuid is None:
+            raise ClientError(
+                "Resumable query has not been started. Call start() first."
+            )
+        payload = {"uuid": self.uuid, "additionalK": additional_k}
+        result = self.client._execute_request(
+            payload=payload, path=RESUMABLE_QUERY_NEXT_PATH
+        )
+        return [QueryResult._from_json(obj) for obj in result]
+
+    async def async_fetch_next(self, additional_k: int) -> List[QueryResult]:
         if not self.uuid:
             raise ClientError(
                 "Resumable query has not been started. Call start() first."
@@ -42,7 +75,19 @@ class ResumableQuery:
         )
         return [QueryResult._from_json(obj) for obj in result]
 
-    async def stop(self) -> str:
+    def stop(self) -> str:
+        if not self.uuid:
+            raise ClientError(
+                "Resumable query has not been started. Call start() first."
+            )
+        payload = {"uuid": self.uuid}
+        result = self.client._execute_request(
+            payload=payload, path=RESUMABLE_QUERY_END_PATH
+        )
+        self.uuid = None
+        return result
+
+    async def async_stop(self) -> str:
         if not self.uuid:
             raise ClientError(
                 "Resumable query has not been started. Call start() first."
