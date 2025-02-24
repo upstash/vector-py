ced4d9003c1b652012effa7f6d01f32d12461221
Metin Dumandag <29387993+mdumandag@users.noreply.github.com>
2025-02-24 19:26:01 +0300

    Allow deleting vectors with prefix or filter (#43)
    * Allow deleting vectors with prefix or filter
    
    We have started supporting deleting vectors with id prefix or
    metadata filter.
    
    * fix type hints

--- tests/core/test_delete.py
+++ tests/core/test_delete.py
@@ -137,3 +137,125 @@ async def test_delete_async(async_index: AsyncIndex, ns: str):
     assert res[0] is None
     assert res[1] is None
     assert res[2] is not None
+
+
+@pytest.mark.parametrize("ns", NAMESPACES)
+def test_delete_prefix(index: Index, ns: str):
+    index.upsert(
+        vectors=[
+            ("id-00", [0.1, 0.2]),
+            ("id-01", [0.1, 0.3]),
+            ("id-10", [0.1, 0.4]),
+            ("id-11", [0.1, 0.5]),
+        ],
+        namespace=ns,
+    )
+
+    result = index.delete(
+        prefix="id-0",
+        namespace=ns,
+    )
+
+    assert result.deleted == 2
+
+    vectors = index.fetch(
+        ids=["id-00", "id-01", "id-10", "id-11"],
+        namespace=ns,
+    )
+    assert len(vectors) == 4
+    assert vectors[0] is None
+    assert vectors[1] is None
+    assert vectors[2] is not None
+    assert vectors[3] is not None
+
+
+@pytest.mark.parametrize("ns", NAMESPACES)
+def test_delete_filter(index: Index, ns: str):
+    index.upsert(
+        vectors=[
+            ("id-00", [0.1, 0.2], {"meta": 0}),
+            ("id-01", [0.1, 0.3], {"meta": 1}),
+            ("id-10", [0.1, 0.4], {"meta": 2}),
+            ("id-11", [0.1, 0.5], {"meta": 3}),
+        ],
+        namespace=ns,
+    )
+
+    result = index.delete(
+        filter="meta >= 2",
+        namespace=ns,
+    )
+
+    assert result.deleted == 2
+
+    vectors = index.fetch(
+        ids=["id-00", "id-01", "id-10", "id-11"],
+        namespace=ns,
+    )
+    assert len(vectors) == 4
+    assert vectors[0] is not None
+    assert vectors[1] is not None
+    assert vectors[2] is None
+    assert vectors[3] is None
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("ns", NAMESPACES)
+async def test_delete_prefix_async(async_index: AsyncIndex, ns: str):
+    await async_index.upsert(
+        vectors=[
+            ("id-00", [0.1, 0.2]),
+            ("id-01", [0.1, 0.3]),
+            ("id-10", [0.1, 0.4]),
+            ("id-11", [0.1, 0.5]),
+        ],
+        namespace=ns,
+    )
+
+    result = await async_index.delete(
+        prefix="id-0",
+        namespace=ns,
+    )
+
+    assert result.deleted == 2
+
+    vectors = await async_index.fetch(
+        ids=["id-00", "id-01", "id-10", "id-11"],
+        namespace=ns,
+    )
+    assert len(vectors) == 4
+    assert vectors[0] is None
+    assert vectors[1] is None
+    assert vectors[2] is not None
+    assert vectors[3] is not None
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("ns", NAMESPACES)
+async def test_delete_filter_async(async_index: AsyncIndex, ns: str):
+    await async_index.upsert(
+        vectors=[
+            ("id-00", [0.1, 0.2], {"meta": 0}),
+            ("id-01", [0.1, 0.3], {"meta": 1}),
+            ("id-10", [0.1, 0.4], {"meta": 2}),
+            ("id-11", [0.1, 0.5], {"meta": 3}),
+        ],
+        namespace=ns,
+    )
+
+    result = await async_index.delete(
+        filter="meta >= 2",
+        namespace=ns,
+    )
+
+    assert result.deleted == 2
+
+    vectors = await async_index.fetch(
+        ids=["id-00", "id-01", "id-10", "id-11"],
+        namespace=ns,
+    )
+    assert len(vectors) == 4
+    assert vectors[0] is not None
+    assert vectors[1] is not None
+    assert vectors[2] is None
+    assert vectors[3] is None
